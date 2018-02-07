7bb5315f01e46a8e467e9050ab107a3477cf9157
Diego Nascimento <diego.nascimento@liferay.com>
2018-02-07 15:39:54 -0300

    Timer and Tomato components done. TODO- Make Tomato component touchable to start/stop

--- src/components/Tomato.js
+++ src/components/Tomato.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import SvgUri from 'react-native-svg-uri';
 import Button from 'react-native-button';
 
 export default class Tomato extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  _onPressSVG(){
 
   }
 
   render() {
     return (
-       <SvgUri
+      <TouchableHighlight
+        onPress={this._onPressSVG}
+      >
+      <SvgUri
             width="200"
             height="200"
             source={{uri:'https://image.flaticon.com/icons/svg/135/135702.svg'}}
         />
+      </TouchableHighlight>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  SvgUri : {
-
-  }
-});
+const styles = StyleSheet.create({});
